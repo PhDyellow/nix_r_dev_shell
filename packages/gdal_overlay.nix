fc2a7298c240fb1f495e3e6b1ae24a7272429d8f
Phil Dyer <phildyer@protonmail.com>
2021-07-08 16:22:48 +1000

    Remove unused package nix files

--- packages/gdal_overlay.nix
+++
@@ -1,6 +0,0 @@
-self: super:
-{
-	libspatialite = (super.callPackage ./libspatialite.nix {});
-	libgeotiff = (super.callPackage ./libgeotiff.nix {});
-	gdal = (super.callPackage ./gdal.nix {});
-}
--- packages/proj_overlay.nix
+++
@@ -1,5 +0,0 @@
-self: super:
-{
-	proj = (super.callPackage ./proj4_v6.nix {}); #this will be merged in to master very soon TODO 2019-07-13
-	proj-datumgrid = (super.callPackage ./proj-datumgrid_v6.nix {});
-}
