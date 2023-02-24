15c16a71ee5676755d0b490ad6f4c518274a0506
Phil Dyer <phildyer@protonmail.com>
2023-02-24 16:02:47 +1000

    drop broken package clustsig, which I am not using anyway

--- r_packages.nix
+++ r_packages.nix
@@ -47,7 +47,7 @@ with pkgs.rPackages; [#All  R packages that I use in my code.
         Hotelling
         ICSNP
         rrcov
-        clustsig
+        #clustsig # currently broken
         Rfast
         fpc
 
