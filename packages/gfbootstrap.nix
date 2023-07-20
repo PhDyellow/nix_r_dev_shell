3dc76df2491d38ffeabe5eb0b50cf2e7cd4ea198
Phil Dyer <phildyer@protonmail.com>
2023-07-20 22:04:06 +1000

    Update GFbootstrap

--- packages/gfbootstrap.nix
+++ packages/gfbootstrap.nix
@@ -24,7 +24,7 @@ pkgs.RmkDerive {
   src = builtins.fetchGit {
     url = "git@github.com:MathMarEcol/gfbootstrap.git";
     ref = "refs/heads/develop";
-    rev =  "2806dfbc76dfd048b2cd63b9759a02d3e250d38e";
+    rev =  "086b6a5a58a431a9cf43c3b2fcf6941365275430";
     };
   depends = sysdepends ++ Rdepends;
 }
