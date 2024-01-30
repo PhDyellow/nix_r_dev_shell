4f61e41f0b9545643914b4c39f9334412e6a4b91
Phil Dyer <phildyer@protonmail.com>
2024-01-30 13:39:32 +1000

    Add crew_cluster

--- r_packages.nix
+++ r_packages.nix
@@ -19,6 +19,7 @@ with pkgs.rPackages; [#All  R packages that I use in my code.
         #provenance
         targets
         crew
+        crew_cluster
         rdtLite
         provSummarizeR
         provViz
