b484a55e05ba21a7d9b28661cd74de99c7b1496f
Phil Dyer <phildyer@protonmail.com>
2024-02-05 14:21:05 +1000

    Fix secretbase build

--- packages/secretbase_github.nix
+++ packages/secretbase_github.nix
@@ -18,10 +18,10 @@ pkgs.RmkDerive {
   version = "0.2.0";
 
   src = pkgs.fetchFromGitHub {
-    owner = "shikikuchuo";
+    owner = "shikokuchuo";
     repo = "secretbase";
     rev = "ced25de47a85c2e4dd27381dabc75fa5135451e8";
-    sha256 =  lib.fakeHash;
+    sha256 =  "sha256-0h/WmGTHrkde6rNeTwbUVgMeEOGrEzYxu56JCubSzfo=";
     fetchSubmodules = true;
   };
 
